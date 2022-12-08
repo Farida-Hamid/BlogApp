945f9a1d7fa75217110de3251114faf3816752a7
Farida-Hamid <faridaaddala@gmail.com>
2022-12-08 09:37:04 +0300

    Create like table

---
+++ db/migrate/20221208063645_create_like.rb
@@ -0,0 +1,10 @@
+class CreateLike < ActiveRecord::Migration[7.0]
+  def change
+    create_table :likes do |t|
+      t.date :updatedat
+      t.date :createdat
+
+      t.timestamps
+    end
+  end
+end
