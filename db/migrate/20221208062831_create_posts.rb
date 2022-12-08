bbeb1a097c9e984d0c040189600f9523c2cfb145
Farida-Hamid <faridaaddala@gmail.com>
2022-12-08 18:56:03 +0300

    Run migration

--- db/migrate/20221208062831_create_posts.rb
+++ db/migrate/20221208062831_create_posts.rb
@@ -1,10 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.references :author, null: false, foreign_key: { to_table: users }
+      t.integer :author_id
       t.text :text
       t.string :title
       t.integer :likescounter
+      t.integer :comments_counter
 
       t.timestamps
     end
--- db/migrate/20221208063645_create_like.rb
+++ db/migrate/20221208063645_create_like.rb
@@ -1,8 +1,8 @@
 class CreateLike < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.references :author, null: false, foreign_key: { to_table: users }
-      t.references :post, null: false, foreign_key: { to_table: posts }
+      t.integer :author_id
+      t.integer :post_id
 
       t.timestamps
     end
