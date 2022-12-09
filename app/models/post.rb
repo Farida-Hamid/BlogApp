2d1d1471d1464473b072af08c4a82b0ab99bfcc2
Farida-Hamid <faridaaddala@gmail.com>
2022-12-09 12:27:11 +0300

    Fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,15 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: true
-  has_many :likes
-  has_many :comments
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :comments, foreign_key: :post_id
+  has_many :likes, foreign_key: :post_id
+
+  private
+
+  def update_post_counter
+    post.increment!(:posts_counter)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
--- db/migrate/20221208062831_create_posts.rb
+++ db/migrate/20221208062831_create_posts.rb
@@ -1,10 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.integer :author_id
       t.text :text
       t.string :title
-      t.integer :likescounter
+      t.integer :likes_counter
       t.integer :comments_counter
 
       t.timestamps
