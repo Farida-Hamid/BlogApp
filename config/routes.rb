d41e5476cd4c8088b0e56cd1ca205f83ea201063
Farida-Hamid <faridaaddala@gmail.com>
2022-12-14 11:10:31 +0300

    Nest routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'users/index'
-  get 'users/show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
+  end
 end
