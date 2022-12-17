455e6ffdc7467f9b7f9a8002b4cec64f890b8590
Farida-Hamid <faridaaddala@gmail.com>
2022-12-17 14:12:54 +0300

    Build page to host the form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,9 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
   end
+
+  def def new
+    @post = Post.new
+  end
+  
 end
