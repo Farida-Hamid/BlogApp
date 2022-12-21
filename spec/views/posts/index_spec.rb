25585543078db8ded7a3a4168108d975499a1a58
Farida-Hamid <faridaaddala@gmail.com>
2022-12-21 17:46:31 +0300

    Fix linter errors

--- spec/views/posts/index_spec.rb
+++ spec/views/posts/index_spec.rb
@@ -22,43 +22,43 @@ RSpec.describe 'post index/#html', type: :feature do
 
     it 'should have the post title' do
       visit("users/#{@user1.id}/posts")
- 
+
       expect(page).to have_content(@user1_post.title)
     end
 
     it 'should have the post body' do
       visit("users/#{@user1.id}/posts")
- 
+
       expect(page).to have_content @user1_post.text
     end
 
     it 'should see some posts comments' do
       visit("users/#{@user1.id}/posts")
- 
+
       expect(page).to have_content @comment.text
     end
 
     it 'should have comment number' do
       visit("users/#{@user1.id}/posts")
- 
+
       expect(page).to have_content "Comments: #{@user1_post.comments_counter}"
     end
 
     it 'should have likes number' do
       visit("users/#{@user1.id}/posts")
- 
+
       expect(page).to have_content "Likes: #{@user1_post.likes_counter}"
     end
 
     it 'should have pagination post fit the view' do
       visit("users/#{@user1.id}/posts")
- 
+
       expect(page).to have_content 'Pagination'
     end
 
     it 'should redirect to a post' do
       visit("users/#{@user1.id}/posts")
-      click_link "Post #"
+      click_link 'Post #'
       expect(current_path).to eq(user_post_path(@user1, @user1_post.id))
     end
   end
