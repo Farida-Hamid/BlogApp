11dd5a7ace42ba78bb2416cac7ce8f873763618e
Quami Killy <killynelly@gmail.com>
2022-12-24 16:43:48 +0000

    modify user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,24 @@
-# class User < ApplicationRecord
+class User < ApplicationRecord
 #   # Include default devise modules. Others available are:
-#   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-#   devise :database_authenticatable, :registerable,
-#          :recoverable, :rememberable, :validatable
-#   # Include default devise modules. Others available are:
-#   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-#   before_save :skip_confirmation!
-#   devise :database_authenticatable, :registerable,
-#          :recoverable, :rememberable, :validatable, :confirmable
-#   has_many :posts, foreign_key: :author_id
-#   has_many :comments, foreign_key: :author_id
-#   has_many :likes, foreign_key: :author_id
+   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+   devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+    Include default devise modules. Others available are:
+    :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+   before_save :skip_confirmation!
+   devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable, :confirmable
+   has_many :posts, foreign_key: :author_id
+   has_many :comments, foreign_key: :author_id
+   has_many :likes, foreign_key: :author_id
 
-#   validates :name, presence: true
-#   validates :postscounter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+   validates :name, presence: true
+   validates :postscounter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
-#   def recent_posts
-#     posts.order(created_at: :desc).last(3)
-#   end
-# end
+   def recent_posts
+     posts.order(created_at: :desc).last(3)
+   end
+end
 
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
